9e722a35f0b9662b32533cfed4d48b79a0ee6184
charlykj <charli0718@gmail.com>
2025-11-09 21:09:14 -0500

    feat(ddl): update database creation script with new tables and constraints

--- content/smart-health/scripts/ddl/01-create-database.sql
+++ content/smart-health/scripts/ddl/01-create-database.sql
@@ -5,7 +5,7 @@
 -- RUN IN POSTGRES - POSTGRES
 
 -- 01. Create user
-CREATE USER carlos WITH PASSWORD '******';
+CREATE USER sm_admin WITH PASSWORD '****';
 
 -- 02. Create database (with ENCODING= 'UTF8', TEMPLATE=Template0, OWNER: music_admin)
 CREATE DATABASE smarthdb WITH 
@@ -17,3 +17,5 @@ CREATE DATABASE smarthdb WITH
 
 -- 03. Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE smarthdb TO sm_admin;
+
+
